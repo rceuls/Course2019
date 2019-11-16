dcf36f98e4127d33d3311dd35fe14edb111b8003
Raf Ceuls <rceuls@gmail.com>
2019-11-16 21:35:48 +0100

    added swagger

--- BreweryFull/Brewery.MVC/Controllers/BreweryAPIController.cs
+++ BreweryFull/Brewery.MVC/Controllers/BreweryAPIController.cs
@@ -19,20 +19,20 @@ namespace Brewery.MVC.Controllers
       _logger = logger;
     }
 
-    [Route("")]
+    [HttpGet("/")]
     public IActionResult GetAllBreweries()
     {
       return Ok(_dataAccess.GetAllBreweries());
     }
 
-    [Route("full")]
+    [HttpGet("full")]
     public IActionResult GetAllBreweriesFull()
     {
       // You return a list here, "not found" is not an issue -- an empty list is still a valid list.
       return Ok(_dataAccess.GetAllBreweriesFull());
     }
 
-    [Route("{id}")]
+    [HttpGet("{id}")]
     public IActionResult GetBrewery(int id)
     {
       // Either you find the brewery or not. If you don't find your resource return a 404 (as per https://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html)
@@ -40,7 +40,7 @@ namespace Brewery.MVC.Controllers
       return brewery == default(Shared.Brewery) ? (IActionResult)NotFound() : Ok(brewery);
     }
 
-    [Route("{id}/beers")]
+    [HttpGet("{id}/beers")]
     public IActionResult GetBreweryBeers(int id)
     {
       // if you don't find the brewery, return a 404. Again, an empty list is an empty list so empty list of beer is a valid result.
@@ -49,7 +49,7 @@ namespace Brewery.MVC.Controllers
     }
 
 
-    [Route("{breweryId}/beers/{beerId}")]
+    [HttpGet("{breweryId}/beers/{beerId}")]
     public IActionResult GetBeerDetails(int breweryId, int beerId)
     {
       // this can return two kinds of 404's; one for the non-existing brewery and one for the non-existing beer.
--- BreweryFull/Brewery.MVC/Startup.cs
+++ BreweryFull/Brewery.MVC/Startup.cs
@@ -1,11 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Brewery.Shared;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -29,6 +24,7 @@ namespace Brewery.MVC
       services.AddDbContext<BreweryContext>(options => options.UseSqlite(Configuration.GetConnectionString("BeerContext")));
       services.AddScoped<IDatabaseInitializer, DatabaseInitializer>();
       services.AddScoped<IBreweryDataAccess, BreweryDataAccess>();
+      services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = "Brewery API", Version = "v1" }); });
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,12 +40,21 @@ namespace Brewery.MVC
         // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
         app.UseHsts();
       }
+
+      app.UseSwagger();
+      app.UseSwaggerUI(c =>
+      {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Brewery API");
+        c.RoutePrefix = string.Empty;
+      });
+
       app.UseHttpsRedirection();
       app.UseStaticFiles();
 
       app.UseRouting();
 
       app.UseAuthorization();
+
 
       app.UseEndpoints(endpoints =>
       {
